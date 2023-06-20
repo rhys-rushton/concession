7dd165906b6bc354bb5d8410f36df218e35763f6
Rhys Rushton <rhysworker@gmail.com>
2023-06-20 20:17:46 +0100

    comments/explainign

--- main.py
+++ main.py
@@ -3,82 +3,78 @@ import numpy as np
 import datetime as dt
 import glob
 import os
+# Helper functions for script
 import helpers
 
+# Load data from the the relevant folders
+# NB the first two lines need to be changed so that they're not specific to remote computer
 transaction_data_path = r'C:\Users\RRushton\Desktop\concession\transactions'
 attendance_data_path = r'C:\Users\RRushton\Desktop\concession\attendance'
-
 transaction_data_csv = glob.glob(os.path.join(transaction_data_path, "*.csv"))
 attendance_data_csv = glob.glob(os.path.join(attendance_data_path, "*.csv"))
 
-print(transaction_data_csv)
-print(attendance_data_csv)
-
+# Read the data from the two folders. 
 transaction_data = helpers.getCsv(transaction_data_csv)
 attendance_data = helpers.getCsv(attendance_data_csv)
-#transaction_data = pd.read_csv(r'C:\Users\RRushton\Desktop\concession\TR Single Line Details KRM 01012021-13012023.csv', encoding='cp1252', low_memory=False)
-#attendance_data = pd.read_csv(r'C:\Users\RRushton\Desktop\concession\Attendance Details (1 Jan 2021 - 31 Dec 2022).csv', encoding='cp1252', low_memory=False)
 
 
+# Sort transaction data by file number and service date. 
 transaction_data = transaction_data.sort_values(by=['File #', 'ServDate'])
-#print(transaction_data)
+
+# Create data frame that has the number of attendances for a ptient on a given date
+# Use file number and date to match billings on the same date for a particular patient. 
 transaction_data_size = transaction_data.groupby(by=['File #', 'ServDate']).size()
-#print(transaction_data_size)
+# Below is a testing line to convert the dataframe with number of billings for a patient on a given day
+# Just for testing purposes.
+#transaction_data_size.to_csv('size.csv',index=True)
 
-
-transaction_data_size.to_csv('size.csv',index=True)
-
+# Get the the attendances that have no 10990/have a 10990
 transaction_data_no_concession = transaction_data[transaction_data.Item != '10990']
 transaction_data_with_concession = transaction_data[transaction_data.Item == '10990']
 
+# Cleaning data so that invoice number is a number and not a string.
 transaction_data_no_concession['Inv #'] = pd.to_numeric(transaction_data_no_concession['Inv #'], errors ='coerce').fillna(-1).astype(np.int64)
 transaction_data_with_concession['Inv #'] = pd.to_numeric(transaction_data_with_concession['Inv #'], errors ='coerce').fillna(-1).astype(np.int64)
 
 
-# Merge everyone and get the people who don't 10990's 
+# Get all attendances that don't have a 10990.
 no_10990s = transaction_data_no_concession.merge(transaction_data_with_concession, on='Inv #', how='left', indicator=True)
 
-
-
+# Map values based on merge values to make clear what status they are. 
 indicator_values = {"left_only": "No 10990", "right_only": "No Matching Item for 10990", "both": "10990 Billed"}
 no_10990s['_merge'] = no_10990s['_merge'].map(indicator_values)
 
+# Potential 10990's is merge column value that doesn't contain '10990 billed'
 potential_10990s = no_10990s[no_10990s._merge != '10990 Billed']
 
-#print(no_10990s)
-#print(potential_10990s)
-
-
 # Clean Up Attendance data and add DOB
+# Here we are getting data that we need from attendance data dataframe. 
+# We just want file number and D.O.B.
 attendance_data_new = attendance_data[['File_Number', 'Date_Of_Birth']]
 attendance_data_new = attendance_data_new.drop_duplicates()
 
+# Merge the data of birth into the relevant patient.
 potential_with_DOB = potential_10990s.merge(attendance_data_new, how='left', left_on='File #_x', right_on='File_Number')
-###Calclulating age
+
+# Calculate the age of the patient based on their date of birth.
+# NB we are not using today's date - we are using the date that they attended the clinic. 
 potential_with_DOB['Date_x'] = pd.to_datetime(potential_with_DOB['Date_x'],format='%d/%m/%Y')
 potential_with_DOB['Date_Of_Birth'] = pd.to_datetime(potential_with_DOB['Date_Of_Birth'],format='%d/%m/%Y', errors='coerce')
-
-
 potential_with_DOB['AGE'] = (potential_with_DOB.Date_x - potential_with_DOB.Date_Of_Birth)
 potential_with_DOB['AGE'] = potential_with_DOB['AGE'] /np.timedelta64(1,'Y')
 
-potential_with_DOB['AGE'] = ((potential_with_DOB['AGE'] >= 66) | (potential_with_DOB['AGE'] < 16))
-
+# Get everyone with a DOB that is greater than 65 or less than 16
+potential_with_DOB['AGE'] = ((potential_with_DOB['AGE'] > 65) | (potential_with_DOB['AGE'] < 16))
 potential_with_DOB = potential_with_DOB[potential_with_DOB['AGE'] == True]
 
-
-#print(potential_with_DOB)
-
-
 # Currently we have all of those who have absolutely no 10990's billed. 
-
 # Now get everyone who is age eligble
 # Find those who may be missing a 10990
-
-# Get age 
+# Merging the dataframe of those without 10990s onto the people who are in the date range.
 transaction_data_no_concession_w_dob = transaction_data_no_concession.merge(attendance_data_new, how='left', left_on='File #', right_on='File_Number')
-#print(transaction_data_no_concession_w_dob)
+
 # Get those who are eligible 
+# Below we are cleaning the data.
 transaction_data_no_concession_w_dob['ServDate'] = pd.to_datetime(transaction_data_no_concession_w_dob['ServDate'],format='%d/%m/%Y')
 transaction_data_no_concession_w_dob['Date_Of_Birth'] = pd.to_datetime(transaction_data_no_concession_w_dob['Date_Of_Birth'],format='%d/%m/%Y', errors='coerce')
 transaction_data_no_concession_w_dob['AGE'] = (transaction_data_no_concession_w_dob.ServDate - transaction_data_no_concession_w_dob.Date_Of_Birth)
